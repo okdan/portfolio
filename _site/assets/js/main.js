f7558872af9f1c552c8b667bcd8419cc603e846f
Dan Carson <cucumbrrr@gmail.com>
2015-03-20 13:00:55 -0500

    redesign of site

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -15,40 +15,6 @@ function isMobile(){
 }
 
 ///////////////////////////////
-// Project Filtering 
-///////////////////////////////
-
-function projectFilterInit() {
-	jQuery('#filterNav a').click(function(){
-		var selector = jQuery(this).attr('data-filter');	
-		jQuery(this).css('outline','none');
-		jQuery('ul#filter .current').removeClass('current');
-		jQuery(this).parent().addClass('current');
-		
-		if(selector == 'all') {
-			jQuery('#projects .thumbs .project.inactive .inside').fadeIn('slow').removeClass('inactive').addClass('active');
-		} else {
-			jQuery('#projects .thumbs .project').each(function() {
-				if(!jQuery(this).hasClass(selector)) {
-					jQuery(this).removeClass('active').addClass('inactive');
-					jQuery(this).find('.inside').fadeOut('normal');
-				} else {
-					jQuery(this).addClass('active').removeClass('inactive');
-					jQuery(this).find('.inside').fadeIn('slow');
-				}
-			});
-		}		
-	
-		if ( !jQuery(this).hasClass('selected') ) {
-			jQuery(this).parents('#filterNav').find('.selected').removeClass('selected');
-			jQuery(this).addClass('selected');
-		}
-	
-		return false;
-	});		
-}
-
-///////////////////////////////
 // Project thumbs 
 ///////////////////////////////
 
@@ -97,27 +63,6 @@ function projectThumbInit() {
 	jQuery(".project.small").css("opacity", "1");	
 }
 
-///////////////////////////////
-// Parallax
-///////////////////////////////
-
-// Calcualte the home banner parallax scrolling
-  function scrollBanner() {
-    //Get the scoll position of the page
-    scrollPos = jQuery(this).scrollTop();
-
-    //Scroll and fade out the banner text
-    jQuery('#bannerText').css({
-      'margin-top' : -(scrollPos/3)+"px",
-      'opacity' : 1-(scrollPos/300)
-    });
-	
-    //Scroll the background of the banner
-    jQuery('#homeBanner').css({
-      'background-position' : 'center ' + (-scrollPos/8)+"px"
-    });    
-  }
-
 
 ///////////////////////////////
 // Initialize
@@ -125,14 +70,5 @@ function projectThumbInit() {
 	
 jQuery.noConflict();
 jQuery(document).ready(function(){
-	
-	if(!isMobile()) {
-		jQuery(window).scroll(function() {	      
-	       scrollBanner();	      
-		});
-	}
 	projectThumbInit();	
-	projectFilterInit();
-	jQuery(".videoContainer").fitVids();
-	jQuery("#homeBanner h2").fitText(1.7, { minFontSize: '24px', maxFontSize: '64px' });	
 });
